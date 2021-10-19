7478e4a52d26579530ffd5db5ef0eadd1d68e7c5
Sultan1901 <sultan.aqeel.elq.js@tuwaiq.edu.sa>
2021-10-19 23:44:09 +0300

    finish

--- task2.js
+++ task2.js
@@ -24,14 +24,66 @@ const p3 = document.querySelectorAll("p");
 bt3.addEventListener("click", bt4);
 function bt4() {
   p3.forEach((element) => {
-    element.style.backgroundColor = "green";
+    element.style.backgroundColor = "brown";
   });
 }
 
 // 4
 const b5 = document.querySelector("#b4");
-const img1 = document.querySelector("img");
+const img1 = document.querySelectorAll("img");
 b5.addEventListener("click", b55);
 function b55() {
-  img1.style.visibility = "hidden";
+  
+    img1.style.visibility = "hidden";
+  ;
 }
+
+// 5
+const b11 = document.querySelector("#b5");
+const chimg1 = document.querySelector("#img1");
+const chimg2 = document.querySelector("#img2");
+b11.addEventListener("click", b66);
+function b66() {
+  chimg1.style.margin = "120px";
+
+  chimg2.style.top = "-500px";
+}
+// 6
+///////////////////
+
+// 7
+
+const alret1 = document.querySelectorAll("img");
+alret1.forEach((ele) => {
+  ele.addEventListener('click' ,b77);
+  function b77() {
+    alert('you clicked')
+  }
+});
+
+// 8 
+
+const inp = document.querySelector('input')
+inp.addEventListener('click', o99)
+function o99(){
+  alert('start write')
+}
+
+// 9
+const aler4 = document.querySelector("body")
+alert('yor page loaded')
+
+// 10
+
+const pcolor = document.querySelectorAll('p')
+pcolor.addEventListener('mouseenter' , h88)
+function h88() {
+  pcolor.style.color = 'red'
+}
+
+
+
+
+  
+
+
